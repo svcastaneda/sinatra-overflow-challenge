4b182579e751a3aa1fabedae8bab036801df1d0c
Phil Thomas <pthomas551@gmail.com>
2016-01-27 14:40:48 -0600

    Complete post question functionality

---
+++ app/controllers/question.rb
@@ -0,0 +1,22 @@
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  if session[:user_id]
+    user = User.find(session[:user_id])
+    question = Question.create(user_id: user.id, title: params[:question_title], body: params[:question_body])
+    redirect "/questions/#{question.id}"
+  else
+    redirect "/user/login"
+  end
+end
+
+# HTTP Request Type Path  View File Description
+# GET '/questions'  '/questions/index'  display a list of all questions
+# GET '/questions/new'  '/questions/new'  return a HTML form for creating new questions
+# POST  '/questions'  '/questions/create' create new questions
+# GET '/questions/:id'  '/questions/show' display specific questions
+# GET '/questions/:id/edit' '/questions/edit' display page with edit questions
+# PUT/PATCH '/questions/:id'  '/questions/update' return a HTML form for editing questions
+# DELETE  '/questions/:id'  '/questions/destroy'  delete specific questions
