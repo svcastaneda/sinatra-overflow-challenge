3d3d61fa008e17f4e299c5c7debc3a8b626a9495
Alex Mitzman <email": null>
2016-01-28 13:40:50 -0600

    Ajaxify comments on questions and answers

--- public/js/application.js
+++ public/js/application.js
@@ -5,32 +5,51 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 	$('.answerCommentForm').hide();
-	
-	// $('form.').on('submit', function( event ) {
-// 		event.preventDefault();
-//     var commentContent = $(this).serialize();
-//
-//     var createRequest = $.ajax({
-//       url: 'comments/',
-//       type: 'POST',
-//       data: commentContent
-//     });
-//
-// 		createRequest.done(function(response){
-//       $('').append(response);
-//     });
-// 	});
-	
+
+	$('body').on('submit', '.answerCommentForm', function( event ) {
+		event.preventDefault();
+    var commentContent = $(this).serialize();
+    var postId = $(this).closest('article').attr('id');
+
+    var createRequest = $.ajax({
+      url: '/answers/'+ postId + '/comments/new',
+      type: 'POST',
+      data: commentContent
+    });
+
+		createRequest.done(function(response){
+			console.log(response)
+      $('.answerDetails').append(response);
+    });
+	});
+
+	$('body').on('submit', '.questionCommentForm', function( event ) {
+		event.preventDefault();
+    var commentContent = $(this).serialize();
+    var postId = $('.question').attr('id');
+
+    var createRequest = $.ajax({
+      url: '/questions/'+ postId + '/comments/new',
+      type: 'POST',
+      data: commentContent
+    });
+
+		createRequest.done(function(response){
+			console.log(response)
+      $('.questionComments').append(response);
+    });
+	});
+
 	$('button.addComment').on('click', function(event) {
 		$('form.newComment').show();
 	});
-	
+
 	$('.container').on('click', '.answer .addComment', function(event) {
 		console.log('wuuuuuut');
 		$(this).hide();
 		$(this).next().show();
 	});
-	
+
 	$('.container').on('submit', '.answer .answerCommentForm', function(event) {
 		event.preventDefault();
 		$(this).hide();
